10388203388310ca6a74f5dea851b619766227ca
Юрий <fedejkin@gmail.com>
2025-03-02 08:27:51 +0300

    Добавлен 0 как полный список перелётов

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -17,71 +17,83 @@ public class Main {
         List<Flight> flights = FlightBuilder.createFlights();  //Получение общего списка перелётов через статический метод
 
         //в методе flightsFilter() происходит последовательное переопределение метода filter() интерфейса FlightFilter:
-        List<List<Flight>> filteredFlights = Filter.flightsFilter(flights, filters);
-
-        int count = 0;
-        for (List<Flight> filter : filteredFlights) {
-
-            count++;
-            if (count == 1) {
-                System.out.println();
-                System.out.println("Отфильтровка перелётов до текущего момента времени: ");
-            }
-            if (count == 2) {
-                System.out.println();
-                System.out.println("Отфильтровка перелётов с датой прилёта раньше даты вылета: ");
-            }
-            if (count == 3) {
-                System.out.println();
-                System.out.println("Отфильтровка перелетов, где общее время, проведённое на земле, превышает два часа: ");
-            }
-            System.out.println(filter.toString());
-        }
+        List<List<Flight>> filteredFlights = Filter.flightsFilter(flights, filters); //В этой ветке не используется
 
         Scanner scanner = new Scanner(System.in);
-        int number = -1;
         while (scanner.hasNextInt()) {
-            scanFilter(flights, filters, number, scanner);
+            scanFilter(flights, filters, scanner);
         }
     }
 
-    private static void scanFilter(List<Flight> flights, List<FlightFilter> filters, Integer number, Scanner scanner) {
-        int number1;
-        int number2;
-        int number3;
-        List<Flight> filteredFlight = new ArrayList<>();
+    private static void scanFilter(List<Flight> flights, List<FlightFilter> filters, Scanner scanner) {
+        Integer number, number1, number2, number3;
+        List<Flight> filteredFlight;
 
         if (scanner.hasNextInt()) {
             number = scanner.nextInt();
+            if (number > 1000) {
+                System.out.println("Такую комбинацию чисел задавать нельзя. Уменьшите до трёх разрядов.");
+                return;
+            }
+            if (number.toString().contains("4") || number.toString().contains("5") || number.toString().contains("6")
+                    || number.toString().contains("7") || number.toString().contains("8") || number.toString().contains("9") || number.toString().contains("00")
+                    || number.toString().contains("01") || number.toString().contains("02") || number.toString().contains("03")
+                    || number.toString().contains("10") || number.toString().contains("20") || number.toString().contains("30")) {
+                System.out.println("Это число содержит цифры, отличные от [1, 2, 3] или имеются нули в середине числа (с краю)");
+                return;
+            }
+            if (number == 0) {
+                nameFilter(number);
+                System.out.println("flights = " + flights);
+            }
             if (number >= 1 && number <= 3) {
                 filteredFlight = Filter.flightsFilter(flights, filters.get(number - 1));
-                System.out.println("Введен фильтр № " + number);
+                nameFilter(number);
                 System.out.println("filteredFlight = " + filteredFlight);
+                return;
             }
             if (number > 10 && number <= 32) {
                 number1 = number / 10;
                 filteredFlight = Filter.flightsFilter(flights, filters.get(number1 - 1));
+                nameFilter(number1);
 
                 number2 = number % 10;
                 filteredFlight = Filter.flightsFilter(filteredFlight, filters.get(number2 - 1));
-                System.out.println("Введен фильтр № " + number);
+                nameFilter(number2);
                 System.out.println("filteredFlight = " + filteredFlight);
-
+                return;
             }
             if (number > 100 && number <= 321) {
                 number1 = number / 100;
                 filteredFlight = Filter.flightsFilter(flights, filters.get(number1 - 1));
+                nameFilter(number1);
 
                 number2 = (number / 10) % 10;
                 filteredFlight = Filter.flightsFilter(filteredFlight, filters.get(number2 - 1));
+                nameFilter(number2);
 
                 number3 = number % 10;
                 filteredFlight = Filter.flightsFilter(filteredFlight, filters.get(number3 - 1));
-                System.out.println("Введен фильтр № " + number);
+                nameFilter(number3);
                 System.out.println("filteredFlight = " + filteredFlight);
             }
         } else {
-            System.out.println("Это не число или число, не входящее в диапазон [1, 2, 3]");
+            System.out.println("Это не число!");
+        }
+    }
+
+    private static void nameFilter(Integer number) {
+        if (number == 0) {
+            System.out.println("Полный список перелётов до фильтрации: ");
+        }
+        if (number == 1) {
+            System.out.println("Отфильтровка перелётов до текущего момента времени: ");
+        }
+        if (number == 2) {
+            System.out.println("Отфильтровка перелётов с датой прилёта раньше даты вылета: ");
+        }
+        if (number == 3) {
+            System.out.println("Отфильтровка перелетов, где общее время, проведённое на земле, превышает два часа: ");
         }
     }
 
